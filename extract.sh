bc66c3b9a4c5889c6df5a28f28c3d38af640917e
Kevin Eid <kevllino@users.noreply.github.com>
2016-04-19 22:02:39 +0100

    Delete extract.sh

--- extract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for year in {2002..2016}
-do 
-  while read location; do 
-	path="ftp://ftp.ncdc.noaa.gov/pub/data/noaa/$year/$location-$year.gz"
-	wget $path
-  done <newba.txt
-done 
